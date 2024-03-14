2621594fcc0eb919ac9731d437902140e9056522
Martin.Machnikowski <machnikowski@gmail.com>
2024-03-14 19:28:21 +0100

    added a modal with props

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{  title }}</h1>
+  <Modal header="Sign up for the Giveaway"/>
 </template>
 
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal
+  },
+  data() {
+    return {
+      title: 'My First Vue App'
+    }
   }
 }
 </script>
@@ -22,5 +29,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid darkgray;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
